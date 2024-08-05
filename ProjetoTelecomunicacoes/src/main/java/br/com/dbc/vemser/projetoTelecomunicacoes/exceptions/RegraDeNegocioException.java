971ab4b6b6c2ca9b8e0376e8f272a4df114d70e2
RenaCV <renacv1996@hotmail.com>
2024-08-05 05:51:23 -0300

    delete

--- ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/exceptions/RegraDeNegocioException.java
+++
@@ -1,9 +0,0 @@
-package br.com.dbc.vemser.projetoTelecomunicacoes.exceptions;
-
-public class RegraDeNegocioException extends Exception{
-
-    public RegraDeNegocioException(String message) {
-        super(message);
-    }
-
-}
